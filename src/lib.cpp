105aad7f73e02c070a8c04f38f01f3e7b8345046
I-mikan-I <67881102+I-mikan-I@users.noreply.github.com>
2024-03-15 20:49:14 +0100

    Refactor; fix bug

--- src/lib.cpp
+++ src/lib.cpp
@@ -6,7 +6,7 @@ module;
 #include <ranges>
 #include <vector>
 #include <iterator>
-#include <utility>    
+#include <utility>
 export module Lib;
 import Elf;
 
@@ -16,7 +16,9 @@ export void print_elf_header(std::filesystem::path elf)
 {
     std::ifstream input{elf, std::ios_base::binary};
     std::vector<unsigned char> file{};
-    copy(subrange(std::istream_iterator<unsigned char>(input), std::istream_iterator<unsigned char>{}), std::back_inserter(file));
+    copy(subrange(std::istreambuf_iterator<char>(input), std::istreambuf_iterator<char>{}) | views::transform([](const char &c)
+                                                                                                              { return std::bit_cast<unsigned char, char>(c); }),
+         std::back_inserter(file));
 
     elf::Elf parsed((owning_view<decltype(file)>(std::move(file))));
 }
